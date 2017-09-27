a38c13d7987c4d8ee21dbe0b05a0391f5e5bfd91
Chris Jarvis <jrvscm@gmail.com>
2017-09-27 16:17:09 -0600

    ajax working. answers rendering.

--- index.js
+++ index.js
@@ -4,37 +4,38 @@ function getDataFromApi (searchTerm){
 		url: `https://www.googleapis.com/youtube/v3/search?`,
 		dataType: 'json',
 		data: {
-			q: `${searchTerm}`,
+			q: `${searchTerm} in:title`,
 			part: `snippet`,
 			maxResults: `20`,
 			key: `AIzaSyDBPGZUKC3yT8dLk8bVaaDmIBApZjyRE-E`
 		},
-		success: function(response) {
-			console.log(response)
-			//renderResult(data);
-		}	
+		success: function(data) {
+			renderResult(data);
+        }	
 	});
 }
 
+function renderResult(data) {
+for(i=0; i<data.items.length; i++) {
+            $('.js-search-results').append(
+            `<h3>Title: ${data.items[i].snippet.title}</h3>
+            <img src="${data.items[i].snippet.thumbnails.medium.url}">
+            <p>${data.items[i].snippet.description}</p>
+            <h4>More from ${data.items[i].snippet.channelTitle}</h4>`);
+}
+}
 
 function watchSubmit() {
 	$('.js-search-form').off().on('click', '.js-search-button', event => {
+		$('.js-search-results').empty();
 		event.preventDefault();
 		const searchTerm = $('.js-query').val();
 		getDataFromApi(searchTerm);
+		$('.js-query').val('');
 	});
 }
 
 
-function renderResult(data) {
-	for(let i=0; i<data.items.length; i++){
-	const searchResult = `<img src="${data.items.i.snippet.thumbnails.medium.url}">`;
-	insertResults(searchResult);
-}
-}
 
-function insertResults(searchResult) {
-	$('.js-search-results').append(searchResult);
-}
 
-watchSubmit();
+$(watchSubmit);
